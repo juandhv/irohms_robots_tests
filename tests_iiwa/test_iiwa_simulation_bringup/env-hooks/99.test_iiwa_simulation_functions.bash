328564978946d9c9ffcd5d7b6659aa4d5d1f3a50
Juan D. Hernandez <HernandezVegaJ@cardiff.ac.uk>
2021-09-07 11:19:04 +0100

    Additional test packages for Kuka iiwa

---
+++ tests_iiwa/test_iiwa_simulation_bringup/env-hooks/99.test_iiwa_simulation_functions.bash
@@ -0,0 +1,11 @@
+# Launch file functions
+
+bringup_name=test_iiwa_simulation
+
+package_path=`rospack find ${bringup_name}_bringup`
+launch_files=$(find ${package_path}/launch -name '*.launch')
+for launch_file in ${launch_files}; do
+    launch_file_name=$(echo ${launch_file##*/} | cut -d"." -f1)
+    eval "${bringup_name//_/-}-${launch_file_name//_/-}() { roslaunch ${launch_file} \$@; }"
+done
+
